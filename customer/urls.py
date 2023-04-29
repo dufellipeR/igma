b995dfe55622a9632c3515955a39e247493d0d00
dufelliper <eduardo.fellipe30@gmail.com>
2023-04-28 21:19:13 -0300

    customer: create

--- customer/urls.py
+++ customer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CustomerList, CustomerDetail
+from .views import CustomerList, CustomerDetail, CustomerCreate
 
 urlpatterns = [
     path('list/', CustomerList.as_view()),
     path('detail/<int:pk>/', CustomerDetail.as_view(), name='customer-detail-by-id'),
     path('detail/', CustomerDetail.as_view(), name='customer-detail-by-cpf'),
+    path('create/', CustomerCreate.as_view())
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
@@ -6,7 +5,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .helpers import sanitize_cpf
+from .helpers import sanitize_cpf, validate_cpf
 from .models import Customer
 from .pagination import StandardResultsSetPagination
 from .serializers import CustomerSerializer
@@ -18,6 +17,23 @@ class CustomerList(generics.ListAPIView):
     pagination_class = StandardResultsSetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'born']
+
+
+class CustomerCreate(generics.CreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        sanitized_cpf = sanitize_cpf(request.data.get('CPF'))
+        if not validate_cpf(sanitized_cpf):
+            return Response({'error': 'Not a valid CPF'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomerDetail(APIView):
@@ -42,9 +58,13 @@ class CustomerDetail(APIView):
 
         sanitized_cpf = sanitize_cpf(cpf)
 
-        obj = self.get_object(None, sanitized_cpf)
-        if obj is not None:
-            serializer = self.serializer_class(obj)
-            return Response(serializer.data)
+        if validate_cpf(sanitized_cpf):
+            obj = self.get_object(None, sanitized_cpf)
+            if obj is not None:
+                serializer = self.serializer_class(obj)
+                return Response(serializer.data)
+            else:
+                return Response({'error': 'No customer found with the specified CPF.'},
+                                status=status.HTTP_404_NOT_FOUND)
         else:
-            return Response({'error': 'No customer found with the specified CPF.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'Not a valid CPF'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
