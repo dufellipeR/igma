f23c832be39df9ca149edc881b025ca3e956298f
dufelliper <eduardo.fellipe30@gmail.com>
2023-04-29 01:23:46 -0300

    readme update

--- customer/tests.py
+++ customer/tests.py
@@ -1,3 +1,141 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+from .models import Customer
+from .views import CustomerList, CustomerDetail, CustomerCreate
+from .helpers import sanitize_cpf, validate_cpf, sum_digits, make_safe_digit
+
+
+class CustomerListTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.factory = APIRequestFactory()
+        Customer.objects.get_or_create(name="Satoshi Nakamoto", born="2008-10-31", CPF="20802113079")
+        Customer.objects.get_or_create(name="Hal Finey", born="1956-05-04", CPF="97088230070")
+        Customer.objects.get_or_create(name="Adam Back", born="1970-07-30", CPF="61876157003")
+        Customer.objects.get_or_create(name="Satoshi Nakamoto", born="2008-10-31", CPF="22349654036")
+        Customer.objects.get_or_create(name="Satoshi Nakamoto", born="2008-10-31", CPF="47878593078")
+        Customer.objects.get_or_create(name="Satoshi Nakamoto", born="2008-10-31", CPF="64570225020")
+
+    def test_get_customers(self):
+        view = CustomerList.as_view()
+        request = self.factory.get(f'list', format='json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 6)
+        self.assertNotEqual(response.data["next"], None)
+
+
+class CustomerCreateTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.factory = APIRequestFactory()
+
+    def test_create_customer(self):
+        view = CustomerCreate.as_view()
+
+        payload = {"name": "Satoshi Nakamoto", "born": "2008-10-31", "CPF": "20802113079"}
+        request = self.factory.post(f'create', payload, format='json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Customer.objects.filter(CPF=payload["CPF"]).exists())
+
+    def test_create_customer_with_cpf_mask(self):
+        view = CustomerCreate.as_view()
+
+        payload = {"name": "Satoshi Nakamoto", "born": "2008-10-31", "CPF": "208.021.130-79"}
+        request = self.factory.post(f'create', payload, format='json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Customer.objects.filter(name="Satoshi Nakamoto").exists())
+
+    def test_create_customer_with_invalid_cpf(self):
+        view = CustomerCreate.as_view()
+
+        payload = {"name": "Satoshi Nakamoto", "born": "2008-10-31", "CPF": "20802113040"}
+        request = self.factory.post(f'create', payload, format='json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+        self.assertTrue(not (Customer.objects.filter(CPF=payload["CPF"]).exists()))
+
+    def test_create_customer_with_invalid_cpf_with_mask(self):
+        view = CustomerCreate.as_view()
+
+        payload = {"name": "Satoshi Nakamoto", "born": "2008-10-31", "CPF": "208.021.130-40"}
+        request = self.factory.post(f'create', payload, format='json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+        self.assertTrue(not (Customer.objects.filter(CPF=payload["CPF"]).exists()))
+
+
+class CustomerDetailTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.factory = APIRequestFactory()
+        cls.customer = Customer.objects.get_or_create(name="Satoshi Nakamoto", born="2008-10-31", CPF="20802113079")
+        Customer.objects.get_or_create(name="Hal Finey", born="1956-05-04", CPF="97088230070")
+        Customer.objects.get_or_create(name="Adam Back", born="1970-07-30", CPF="61876157003")
+
+    def test_retrieve_customer_by_pk(self):
+        view = CustomerDetail.as_view()
+        request = self.factory.get(f'detail/{self.customer[0].id}', format='json')
+        response = view(request, pk=self.customer[0].id)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.customer[0].id)
+        self.assertEqual(response.data['name'], self.customer[0].name)
+        self.assertEqual(response.data['CPF'], self.customer[0].CPF)
+
+    def test_retrieve_customer_by_cpf(self):
+        view = CustomerDetail.as_view()
+
+        payload = {"CPF": self.customer[0].CPF}
+        request = self.factory.post(f'detail', payload, format='json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.customer[0].id)
+        self.assertEqual(response.data['name'], self.customer[0].name)
+        self.assertEqual(response.data['CPF'], self.customer[0].CPF)
+
+    def test_retrieve_customer_by_cpf_with_mask(self):
+        view = CustomerDetail.as_view()
+
+        payload = {"CPF": "208.021.130-79"}
+        request = self.factory.post(f'detail', payload, format='json')
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.customer[0].id)
+        self.assertEqual(response.data['name'], self.customer[0].name)
+        self.assertEqual(response.data['CPF'], self.customer[0].CPF)
+
+
+class HelpersTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.cpf_to_sanitize = "111.444.777-35"
+        cls.already_sanitized = "11144477735"
+        cls.invalid_cpf = "11144477705"
+
+    def test_sanitize_cpf(self):
+        self.assertEqual(sanitize_cpf(self.cpf_to_sanitize), self.already_sanitized)
+
+    def test_validate_cpf(self):
+        self.assertTrue(validate_cpf(self.already_sanitized))
+        self.assertTrue(not (validate_cpf(self.invalid_cpf)))
+        self.assertTrue(not (validate_cpf(self.already_sanitized[:5])))
+
+    def test_sum_digits(self):
+        self.assertEqual(sum_digits(self.already_sanitized[:9]), 162)
+        self.assertEqual(sum_digits(self.already_sanitized[:10]), 204)
+
+    def test_make_safe_digit(self):
+        self.assertEqual(make_safe_digit(162), '3')
+        self.assertEqual(make_safe_digit(231), '0')
--- readme.md
+++ readme.md
@@ -1,1 +1,88 @@
-#Titul
+# IGMA
+
+This project is a Django REST Framework (DRF) API built with Docker
+
+## Requirements
+
+To run this project, you'll need the following:
+
+- Docker
+- Docker Compose
+
+## Overview
+
+- Tests: *tests.py*
+- Endpoints: *views.py*
+- CPF Validator: *helpers.py*
+- Schema: *models.py*
+
+## Getting Started
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/dufellipeR/igma.git
+    cd igma
+    ```
+
+2. Build and run the Docker containers:
+
+    ```sh
+    docker-compose up --build
+    ```
+
+3. Create the Django database tables:
+
+    ```sh
+    docker-compose exec server python manage.py migrate
+    ```
+
+
+5. The API should now be available at [http://localhost:8000/](http://localhost:8000/).
+
+## Development
+
+To start a development server:
+
+1. Build and run the Docker containers:
+
+    ```sh
+    docker-compose up --build
+    ```
+
+2. Run Django's development server:
+
+    ```sh
+    docker-compose exec server python manage.py runserver 0.0.0.0:8000
+    ```
+
+3. The API should now be available at [http://localhost:8000/api/](http://localhost:8000/api/).
+
+## Testing
+
+To run the tests:
+
+```sh
+docker-compose exec server python manage.py test
+```
+
+Location: *tests.py*
+
+## Configuration
+
+### Environment Variables
+
+This project uses environment variables to configure the database and other settings. For simplicity sake, the enviroment variables are already set on docker-compose.yml
+
+## Technologies Used
+
+This project was developed using the following technologies:
+
+- Python 3.10.9
+- Django 4.2
+- Django REST Framework 3.14
+- PostgreSQL 13.3
+
+The project also uses other libraries and tools that can be found in the `requirements.txt` file. 
+
+For more information on how to run the project, please refer to the [Getting Started](#getting-started) section of this README.
